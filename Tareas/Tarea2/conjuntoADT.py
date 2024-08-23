c40a196368654f152a0d59c2549ff66909801102
kno4 <milokno4@gmail.com>
2024-08-22 21:48:57 -0600

    Tarea2 update

--- Tareas/Tarea2/conjuntoADT.py
+++ Tareas/Tarea2/conjuntoADT.py
@@ -3,25 +3,20 @@ class ConjuntoADT:
         self._elementos = []
 
     def longitud(self):
-        """Devuelve el número de elementos en el conjunto."""
         return len(self._elementos)
 
     def contiene(self, elemento):
-        """Devuelve True si el elemento está en el conjunto, False de lo contrario."""
         return elemento in self._elementos
 
     def agregar(self, elemento):
-        """Agrega un elemento al conjunto si no está presente."""
         if not self.contiene(elemento):
             self._elementos.append(elemento)
 
     def eliminar(self, elemento):
-        """Elimina el elemento del conjunto si está presente."""
         if self.contiene(elemento):
             self._elementos.remove(elemento)
 
     def equals(self, otro_conjunto):
-        """Comprueba si el conjunto actual y otro_conjunto tienen los mismos elementos."""
         if self.longitud() != otro_conjunto.longitud():
             return False
         for elemento in self._elementos:
@@ -30,14 +25,12 @@ class ConjuntoADT:
         return True
 
     def es_subconjunto(self, otro_conjunto):
-        """Devuelve True si todos los elementos del conjunto están en otro_conjunto."""
         for elemento in self._elementos:
             if not otro_conjunto.contiene(elemento):
                 return False
         return True
 
     def union(self, otro_conjunto):
-        """Retorna un nuevo conjunto que es la unión del conjunto actual y otro_conjunto."""
         nuevo_conjunto = ConjuntoADT()
         # Agregar todos los elementos del conjunto actual
         nuevo_conjunto._elementos = self._elementos.copy()
@@ -48,7 +41,6 @@ class ConjuntoADT:
         return nuevo_conjunto
 
     def interseccion(self, otro_conjunto):
-        """Retorna un nuevo conjunto que es la intersección del conjunto actual y otro_conjunto."""
         nuevo_conjunto = ConjuntoADT()
         for elemento in self._elementos:
             if otro_conjunto.contiene(elemento):
@@ -56,7 +48,6 @@ class ConjuntoADT:
         return nuevo_conjunto
 
     def diferencia(self, otro_conjunto):
-        """Retorna un nuevo conjunto con los elementos del conjunto actual que no están en otro_conjunto."""
         nuevo_conjunto = ConjuntoADT()
         for elemento in self._elementos:
             if not otro_conjunto.contiene(elemento):
@@ -64,5 +55,4 @@ class ConjuntoADT:
         return nuevo_conjunto
 
     def __str__(self):
-        """Devuelve una representación en cadena del conjunto."""
         return "{" + ", ".join(str(elemento) for elemento in self._elementos) + "}"
